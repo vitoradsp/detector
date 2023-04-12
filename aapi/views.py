4b358709275ba82779f22b90b502621b139748f9
vitoradsp <vitoradsp@gmail.com>
2023-04-12 02:54:11 -0300

    first

--- aapi/views.py
+++ aapi/views.py
@@ -1,4 +1,4 @@
-from rest_framework import mixins, permissions, viewsets
+from rest_framework import viewsets
 from rest_framework.response import Response
 from aapi.models import Recebedor
 from aapi.serializers import RecebedorSerializer
@@ -6,21 +6,22 @@ from PIL import Image
 import numpy as np
 import cv2
 from io import BytesIO
-from django.middleware import csrf
-from django.http import HttpResponse
+from urllib.request import urlopen
 
 class RecebedorViewSet(viewsets.ModelViewSet):
     queryset = Recebedor.objects.all()
     serializer_class = RecebedorSerializer
     def create(self, request):
         if request.method == 'POST':
-            logo_path = 'media/logo.png'
             imgp = Image.open(request.FILES['img'])
+            logo_path = urlopen('https://lh3.googleusercontent.com/pw/AMWts8ANE384tAfx5LraQeLwo6kCKeXNX6vq1Yr1w4gJ1b0eosTdwe6UGJl6Ka9aijhq4ufyEWosvkKgmJsDtoriJ7isW8TzOhzet6W4fZcQwmiYEkMKBntuizF665EGQVLRRCbxLTn2io4CBCXDBAuycwIX7rbfzXJ81E_2clYE1CwfU9WKDtKKNelv10JPKUMWMJM3qy5RyDR4yOr0ZB94IUpKnOoeo0iJ4HjnulBEa1VnVORc1jhsfesSDf-F-jJ-kbaSCWjilYjC0xXj31UTxlChJxNGsCuXh1wvspKHjaUjYASx5U2kgs1wPINXaFQEIRVjjwrjZOfBsEQnZ9jOSK1FUE1zCaRjJHK-9QkU1qGTxxd6InEiNSfnC6UorYQNYiDUJlbHIYO0zzxfxhlMJPEz8sdRUuAqp3yGW8Lnjl2Ei9sqPSCq31oBYqr5eUJwQa7rQqFgE6l_9bx4e20q_ZBsszhauLl0TGBpwMgh1UmfGAwTe90WGzYmQjG7owkad_Q6q2-BHDrIQ3c8CTY4tK8N4WXJ0DJf13NdrkGWw8YESwvnCeQQq-bsNxjsTPjlITNFi-yH5axDd2frc-EeNKof7CURzrDCl3kg_rlrpm3PTYYvaFnp2EitNE8j81bITay2Tyd-JGn28bZ6-gW2CKq6TSargABV5N9i8atstRR9ZTprkK_QvFCgTjwIRlvRFKAqbImcaJ1syOJyVT6ycqOv5zQpBem1XyZsGFjo-WGaJcPUtvOfsLbh0Q2PGfJq00c7p9tZF7IBXAaEX4S7T52nCAzhjOSlbb5rYaeS_vTqIRRSO08chLQIgp8ieeg6nmNbjYeNWuJQ3mii-5OADdPpuzRs8IsDSgMXbjlGund9vADAqKe2nyGLK3pmvyTjoXTTYTpB3TonSoJG3kBCuJVWwYuS-5ot347-bEY5scmTt1Bg1W8aRlCuGUMtWW0sfZxv3wExytJAbcne2jrO50YkgS5KNxbY4bSRJZc0VbmIjyBZhLDtXmu3aAcWyHaEK2JRU_gU6iaBZmgeLnSJEcpECrcFxBLG0CUeB_hG44nBLMx0ik9Oxws1vTwPIFaIP3M_RL6JCdy43uVC1HyDpKIUdY7_bh7BsV6-V2IEJ1XdkHMD0tp_HV0jOFtrjTqZ-AhNLCKe3U0lBQ=w176-h34-s-no?authuser=0')
+            logo_data = logo_path.read()
+            logo_arr = np.array(bytearray(logo_data), dtype=np.uint8)
             img_bin = BytesIO()
             imgp.save(img_bin, format='PNG')
             img_data = img_bin.getvalue()
             imgf = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_GRAYSCALE)
-            mainp = cv2.imread(logo_path, 0)
+            mainp = cv2.imdecode(logo_arr, 0)
             template2 = imgf.copy()
             w,h = mainp.shape[::-1]
             result = cv2.matchTemplate(template2, mainp, cv2.TM_CCOEFF_NORMED)
@@ -38,6 +39,3 @@ class RecebedorViewSet(viewsets.ModelViewSet):
                 return Response('Dectectado')
             else:
                 return Response('Nada')
-        elif request.method == 'GET':
-            csrftoken = csrf.get_token(request)
-            return HttpResponse(csrftoken)
